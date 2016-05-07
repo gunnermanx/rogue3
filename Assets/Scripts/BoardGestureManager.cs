9c6f88d6aca58a69d0cfa452fc8530a572f27749
gunnermanx <yuen.alb@gmail.com>
2016-05-07 10:48:56 -0700

    Fixed mobile touch input and chose better icons

--- Assets/Scripts/BoardGestureManager.cs
+++ Assets/Scripts/BoardGestureManager.cs
@@ -31,13 +31,13 @@ public class BoardGestureManager : MonoBehaviour {
 
 		if ( _currentState == GestureState.None ) {
 			Vector3 position;
-			#if UNITY_EDITOR
+#if UNITY_EDITOR
 			if ( Input.GetMouseButtonDown(0) ) {
 				position = Input.mousePosition;
-			#else
+#else
 			if ( Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began ) {
 				position = Input.GetTouch(0).position;
-			#endif
+#endif
 				Ray ray =  Camera.main.ScreenPointToRay( position );
 				RaycastHit hit;
 				if ( Physics.Raycast( ray, out hit, Mathf.Infinity, TileMask ) ) {
@@ -54,8 +54,13 @@ public class BoardGestureManager : MonoBehaviour {
 			}
 		}
 		else if ( _currentState == GestureState.DraggingTile ) {
+#if UNITY_EDITOR
 			if ( Input.GetMouseButtonUp(0) ) {
 				Vector3 position = Input.mousePosition;
+#else
+			if ( Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended ) {
+				Vector3 position = Input.GetTouch(0).position;
+#endif
 				Ray ray =  Camera.main.ScreenPointToRay( position );
 				Tile targetTile = null;
 
@@ -75,11 +80,18 @@ public class BoardGestureManager : MonoBehaviour {
 					onDropTile( targetTile );
 				}
 			}
+#if UNITY_EDITOR
 			if ( Input.GetMouseButton(0) ) {
-				Vector3 mousePos = Input.mousePosition;
-				mousePos.z = Tile.Z_DEPTH - Camera.main.transform.position.z;
-				Vector3 worldPos = Camera.main.ScreenToWorldPoint( mousePos );
-
+				Vector3 position = Input.mousePosition;
+				position.z = Tile.Z_DEPTH - Camera.main.transform.position.z;
+				Vector3 worldPos = Camera.main.ScreenToWorldPoint( position );
+#else
+			if ( Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved ) {
+				Vector3 position = Input.GetTouch(0).position;
+				position.z = Tile.Z_DEPTH - Camera.main.transform.position.z;
+				Vector3 worldPos = Camera.main.ScreenToWorldPoint( position );
+#endif
+				
 				if ( onDragTile != null ) {
 					onDragTile( worldPos );
 				}
