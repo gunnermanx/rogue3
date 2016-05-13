7377e05dd1faa9b25ebbff45566c5ff49b0fb998
Albert Yuen <ayuen@zynga.com>
2016-05-13 09:41:27 -0700

    Simple dialog stuff

--- Assets/Scripts/UI/BaseDialog.cs
+++ Assets/Scripts/UI/BaseDialog.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public abstract class BaseDialog : MonoBehaviour {
+public abstract class BaseDialog : MonoBehaviour, IPointerClickHandler {
+
+	[SerializeField]
+	protected GameObject _touchToExitObject; 
 
 	public abstract string GetDialogId();
 
+	#region IPointerClickHandler implementation
+	public void OnPointerClick( PointerEventData eventData ) {
+		GameObject go = eventData.pointerCurrentRaycast.gameObject;
+		if ( _touchToExitObject != null && _touchToExitObject == go ) {
+			UIManager.Instance.CloseDialog( GetDialogId() );
+		}
+	}
+	#endregion
 }
 
